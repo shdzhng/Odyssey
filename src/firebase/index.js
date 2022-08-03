b4a5d39dcac299e372a1a09022e10677ba88d93a
Shou Zhang <shoud.zhang@gmail.com>
2022-08-03 15:21:52 -0700

    implement accept friend request feature

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -14,6 +14,7 @@ import {
   removeFriend,
   toggleOnline,
   sendTripInv,
+  acceptFriendReq,
   acceptTripInv,
   addTrip,
   removeTrip,
@@ -111,6 +112,7 @@ export function AuthProvider({ children }) {
     friendsOffline,
     incomingFriends,
     outgoingFriends,
+    acceptFriendReq,
 
     /// user
     userRef,
